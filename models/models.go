97c6cfef49284189e08051e522c90814d3c88594
jszafran <jszafran.pv@gmail.com>
2022-07-16 21:47:51 +0200

    Add models for api response.

--- models/models.go
+++ models/models.go
@@ -19,6 +19,33 @@ type SourceAggregatedTopHeadlines struct {
 	TopHeadlines []TopHeadline
 }
 
+type SourceApiResponse struct {
+	Status  string `json:"status"`
+	Sources []struct {
+		Id          string `json:"id"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		Url         string `json:"url"`
+		Category    string `json:"category"`
+		Language    string `json:"language"`
+		Country     string `json:"country"`
+	} `json:"sources"`
+}
+
+type TopHeadlinesApiResponse struct {
+	Status       string `json:"status"`
+	TotalResults int    `json:"totalResults"`
+	Articles     []struct {
+		Source struct {
+			Id   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"source"`
+		Author      string `json:"author"`
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	} `json:"articles"`
+}
+
 type NewsAPIClient interface {
 	GetSources(language string) []NewsSource
 	GetTopHeadlines(sources []NewsSource) []SourceTopHeadline
