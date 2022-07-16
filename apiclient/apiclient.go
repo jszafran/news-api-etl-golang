cccd2781776e48c85f95e5a2156b177caa312f23
jszafran <jszafran.pv@gmail.com>
2022-07-16 16:56:46 +0200

    Add test for http client api url.

--- apiclient/apiclient.go
+++ apiclient/apiclient.go
@@ -3,23 +3,35 @@ package apiclient
 import (
 	"net/http"
 	"newsapietl/models"
+	"time"
 )
 
-type NewsApiHTTPClient struct {
+type ApiAuthDetails struct {
 	apiKey string
 	apiUrl string
-	client http.Client
 }
 
-func doRequest(path string) {
+type NewsApiHTTPClient struct {
+	apiAuthDetails ApiAuthDetails
+	client         http.Client
+}
 
+func MakeNewsApiHTTPClient(apiAuthDetails ApiAuthDetails) NewsApiHTTPClient {
+	client := http.Client{Timeout: time.Second * 5}
+	if string(apiAuthDetails.apiUrl[len(apiAuthDetails.apiUrl)-1]) != "/" {
+		apiAuthDetails.apiUrl += "/"
+	}
+	return NewsApiHTTPClient{apiAuthDetails, client}
+}
+
+func (n *NewsApiHTTPClient) doRequest(path string) http.Response {
+	return http.Response{}
 }
 
 func (n *NewsApiHTTPClient) GetSources(language string) []models.NewsSource {
-
+	return []models.NewsSource{}
 }
 
 func (n *NewsApiHTTPClient) GetTopHeadlines(sources []models.NewsSource) []models.SourceTopHeadline {
-
+	return []models.SourceTopHeadline{}
 }
-
---
+++ apiclient/apiclient_test.go
@@ -0,0 +1,25 @@
+package apiclient
+
+import "testing"
+
+func TestMakeNewsApiHTTPClient(t *testing.T) {
+	type test struct {
+		apiUrl      string
+		expectedUrl string
+	}
+
+	tests := []test{
+		{"https://someapi.com/", "https://someapi.com/"},
+		{"https://someapi.com", "https://someapi.com/"},
+	}
+
+	for _, test := range tests {
+		client := MakeNewsApiHTTPClient(ApiAuthDetails{
+			apiKey: "apikey",
+			apiUrl: test.apiUrl,
+		})
+		if client.apiAuthDetails.apiUrl != test.expectedUrl {
+			t.Fatalf("Expected %v, got %v", test.expectedUrl, client.apiAuthDetails.apiUrl)
+		}
+	}
+}
